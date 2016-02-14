853e3fdac6e535517f967213991ee9df3c5ea7a7
Ad van der Veer <advanderveer@gmail.com>
2016-02-14 15:04:25 +0100

    use curl for github releases

--- make.sh
+++ make.sh
@@ -11,16 +11,6 @@ function print_help {
 		| expand -t 20
 }
 
-# function run_release { #publish a new release to github
-#   git tag v`cat VERSION` || true
-#   git push --tags
-#
-#   : "${GITHUB_TOKEN:?GITHUB_TOKEN environment variable needs to be set in order to test}"
-#   run_build_container
-#   docker build -t microfactory/zero:linux-release -f Dockerfile.release .
-#   docker run -it --rm -e "GITHUB_TOKEN=$GITHUB_TOKEN" microfactory/zero:linux-release
-# }
-
 function run_build { #build linux binary in a Docker container
 	docker build -f build.Dockerfile -t microfactory/zero:build-`cat VERSION` .
   docker run -it -v $PWD:/src/github.com/microfactory/zero --rm microfactory/zero:build-`cat VERSION`
@@ -37,28 +27,38 @@ function run_run { #run a Linux test environment
     microfactory/zero:`cat VERSION` -start-daemon -name=test-member $ZT_NET $ZT_TOKEN
 }
 
-# This is expected to be run INSIDE a container
-# function do_release {
-#   : "${GITHUB_TOKEN:?GITHUB_TOKEN environment variable needs to be set in order to test}"
-#   printf "Drafing release...\n"
-#   github-release release \
-#       --user microfactory \
-#       --repo zero \
-#       --tag v`cat VERSION` \
-#       --pre-release
-#
-#   printf "Uploading...\n"
-#   github-release upload \
-#       --user microfactory \
-#       --repo zero \
-#       --tag v`cat VERSION` \
-#       --name zero \
-#       --file /usr/local/bin/zero
-# }
+function run_draft-release { #draft a release for the current version
+	: "${GITHUB_TOKEN:?GITHUB_TOKEN environment variable needs to be set}"
+	printf "Tagging Commit and Push Tag...\n"
+	git tag v`cat VERSION` || true
+	git push --tags
+	printf "Drafting Release...\n"
+	version=$(cat VERSION)
+	json=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $version $version $version)
+	curl -H "Authorization: token $GITHUB_TOKEN" \
+			 --data "$json" https://api.github.com/repos/microfactory/zero/releases
+}
+
+function run_upload-release { #upload binaries for current release
+	: "${GITHUB_TOKEN:?GITHUB_TOKEN environment variable needs to be set}"
+	version=$(cat VERSION)
+	relid=$(curl -H "Authorization: token $GITHUB_TOKEN" \
+		 -H "Accept: application/vnd.github.manifold-preview" \
+		 "https://api.github.com/repos/microfactory/zero/releases" \
+		 	| jq -c --arg version "$version" '.[] | select(.tag_name | contains($version)) | .id')
+
+	printf "Uploading Files...\n"
+	curl -H "Authorization: token $GITHUB_TOKEN" \
+	     -H "Accept: application/vnd.github.manifold-preview" \
+	     -H "Content-Type: application/octet-stream" \
+	     --data-binary @linux-amd64/zero \
+	     "https://uploads.github.com/repos/microfactory/zero/releases/$relid/assets?name=zero_linux-amd64"
+}
 
 case $1 in
 	"do-release") do_release ;;
-  # "release") run_release ;;
+  "draft-release") run_draft-release ;;
+	"upload-release") run_upload-release ;;
 	"build") run_build ;;
 	"run") run_run ;;
 	*) print_help ;;
